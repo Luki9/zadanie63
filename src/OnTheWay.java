38c4f2386f71613fbaa452db6a7676553328180e
lukasz <robson27-r@hotmail.com>
2019-02-28 13:48:46 +0100

    w trakcie 5

--- src/OnTheWay.java
+++ src/OnTheWay.java
@@ -1,6 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Scanner;
 
 class OnTheWay {
+
+
+    File file = new File("orders.csv");
+    Scanner scanner;
+
+    {
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
 
 
