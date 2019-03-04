913e0c6057d154e9ad4c3136c4437697baffbfee
lukasz <robson27-r@hotmail.com>
2019-03-04 14:28:02 +0100

    w trakcie 10....

--- src/Main2.java
+++ src/Main2.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 class Main2 {
 
-    static void readData(List<Order>orders){
+    static void readData(List<Order> orders) {
         Scanner scanner = new Scanner(System.in);
         int data = 0;
         System.out.println("Zamówienie:" + orders + "\n");
@@ -14,19 +14,87 @@ class Main2 {
             System.out.println("1: Sortuj po nazwie,\n2: Sortuj po cenie,\n3: Sortuj po statusie\n4: Zmiana zamówienia\n5: Dodaj Zamówienie\n6: Anuluj\n7: Koniec");
             try {
                 data = scanner.nextInt();
-            }catch (InputMismatchException e){
+            } catch (InputMismatchException e) {
                 System.err.println("Błąd. Podaj liczbę [1.4]");
                 scanner.nextLine();
             }
-            switch (data){
+            switch (data) {
                 case 1:
-              Main2.sortByStatus(orders);
+                    OrderCompleted.getOrderStatus(orders +);
+                    System.out.println("Sortuj po nazwie");
+                    System.out.println("Zamówienia:" + orders + "\n");
+                    break;
+                case 2:
+                    OrderCompleted.getOrderStatus(orders +);
+                    System.out.println("Sortuj po cenie:");
+                    System.out.println("Zamówienie:" + "\n");
+                    break;
+                case 3:
+                    OrderCompleted.getOrderStatus(orders +);
+                    System.out.println("Sortuj po statusie");
+                    System.out.println("Zamówienie:" + orders + "\n");
+                    break;
+                case 4:
+                    changeStatus(orders);
+                    System.out.println("Zamówienie" + orders + "\n");
+                    break;
+                case 5:
+                    readData(orders);
+                    System.out.println("Zamówienie" + orders + "\n");
+                    break;
+                case 6:
+                    cancelOrder(orders);
+                    System.out.println("Zamówienie:" + orders + "\n");
+                    break;
+                case 7:
+                    break;
+
             }
 
-        }while (data !=7);
+        } while (data != 7);
 
     }
 
-    private static void sortByStatus(List<Order> orders) {
+    private static void addOrder(List<Order> orders) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("Nazwa produktu");
+
+            System.out.println("Wartość zaówineia");
+            double price = scanner.nextDouble();
+            scanner.nextLine();
+            OrderCompleted.getOrderStatus(orders, new Order(name, price, OrderStatus.COMPLETED));
+            System.out.println("Zamówienie gotowe");
+        } catch (InputMismatchException e) {
+            System.err.println("Błędne dane");
+
+        }
+    }
+
+    private static void cancelOrder(List<Order> orders) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Id zamówineia do anulowania");
+        try {
+            int id = scanner.nextInt();
+            scanner.nextLine();
+            OrderCompleted. (id, orders);
+        } catch (InputMismatchException e) {
+            System.out.println("Błędne dane");
+
+        }
+    }
+
+    private static void changeStatus(List<Order> orders) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            int id = scanner.nextInt();
+            scanner.nextLine();
+            OrderCompleted.changeOrderStatus(id, orders);
+        } catch (InputMismatchException e) {
+            System.out.println("Błędne dane");
+
+        }
+
+
     }
 }
--- src/OrderCompleted.java
+++ src/OrderCompleted.java
@@ -7,8 +7,9 @@ class OrderCompleted {
         orders.add(new Order(orders.size() + 1, order.getName(), order.getPrice(), order.getOrderStatus()));
 
     }
+
     static boolean changeOrderStatus(int id, List<Order> orders) {
-        if (!changeOrderStatus(id, orders)) {
+        if (!checkID(id, orders)) {
             System.out.println(";");
         } else {
             for (int i = 0; i < orders.size(); i++) {
@@ -33,19 +34,20 @@ class OrderCompleted {
                 }
 
             }
+
         }
-        static boolean cancelOrder(int id List<Order> orders){
-            if (!changeOrderStatus(id, orders))
+        static boolean cancelOrderStatus(int id  List<Order> orders){
+            if (!checkID(id, orders))
                 System.out.println("Stan zamówiena" + id + ":");
-            else{
+            else {
                 for (int i = 0; i < orders.size(); i++) {
-                    if (orders.get(i).getId() == id){
-                        if(orders.get(i).getOrderStatus() == OrderStatus.ORDER || orders.get(i).getOrderStatus() == OrderStatus.PREPARED_FOR_SHIPPING){
+                    if (orders.get(i).getId() == id) {
+                        if (orders.get(i).getOrderStatus() == OrderStatus.ORDER || orders.get(i).getOrderStatus() == OrderStatus.PREPARED_FOR_SHIPPING) {
                             orders.get(i).setOrderStatus(OrderStatus.Canceled);
                             System.out.println("Stan zamówienia " + (i + 1) + "Anulowane");
 
-                        }else if(orders.get(i).getOrderStatus() != OrderStatus.PREPARED_FOR_SHIPPING && orders.get(i).getOrderStatus() == OrderStatus.PREPARED_FOR_SHIPPING){
-                            System.out.println("Stan zamówienia" + (i + 1 ) + "Koniec usługi");
+                        } else if (orders.get(i).getOrderStatus() != OrderStatus.PREPARED_FOR_SHIPPING && orders.get(i).getOrderStatus() == OrderStatus.PREPARED_FOR_SHIPPING) {
+                            System.out.println("Stan zamówienia" + (i + 1) + "Koniec usługi");
 
                         }
 
@@ -55,9 +57,23 @@ class OrderCompleted {
             }
 
         }
-
-
-
+        static void sortByName(List<Order>orders) {
+            Collections.sort(orders, new NameComparator());
+        }
+        static void sortByPrice(List<Order> orders){
+            Collections.sort(orders, new PriceComparator());
 
     }
-}
+        static void sortByStatus(List<Order> orders){
+            Collections.sort(orders, new OrderStatusComparator());
+        }
+
+    private static boolean checkID(int id, List<Order> orders){
+            for (orders.get(i).getId() == checkID)
+                return true;
+        }
+    return false;
+    }
+
+    }
+
